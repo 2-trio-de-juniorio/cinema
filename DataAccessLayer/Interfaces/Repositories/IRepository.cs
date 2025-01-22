9d0b91e0175771d85e2ab57c07021e34d11704ec
stksl <via4eslaw.roga5ew@gmail.com>
2025-01-22 21:07:38 +0200

    fixes

--- DataAccessLayer/Interfaces/Repositories/IRepository.cs
+++ DataAccessLayer/Interfaces/Repositories/IRepository.cs
@@ -1,15 +1,14 @@
 namespace DataAccessLayer.Interfaces;
 
+// Currently implemented as an interface. In future, class-based repositories might be used
 public interface IRepository<TEntity> : IDisposable
     where TEntity : class 
 {
     Task<TEntity?> GetByIdAsync(int id);
     Task<List<TEntity>> GetAllAsync();
-
     void Add(TEntity entity);
     void Update(TEntity entity);
     void Remove(TEntity entity);
-    Task RemoveAsync(int id);
-
+    Task RemoveByIdAsync(int id);
     Task SaveAsync();
 }
--- DataAccessLayer/Repositories/Repository.cs
+++ DataAccessLayer/Repositories/Repository.cs
@@ -6,43 +6,53 @@ namespace DataAccessLayer.Repositories;
 internal sealed class Repository<TEntity> : IRepository<TEntity>
     where TEntity : class
 {
-    private readonly AppDbContext _ctx;
+    private readonly AppDbContext _dbContext;
     private readonly DbSet<TEntity> _entities;
-    public Repository(AppDbContext ctx)
+    public Repository(AppDbContext dbContext)
     {
-        _ctx = ctx;
-        _entities = ctx.Set<TEntity>();
+        _dbContext = dbContext;
+        _entities = dbContext.Set<TEntity>();
     }
 
 
-    public Task<TEntity?> GetByIdAsync(int id) => _entities.FindAsync(id).AsTask();
-    public Task<List<TEntity>> GetAllAsync() => _entities.ToListAsync();
+    public Task<TEntity?> GetByIdAsync(int id) 
+    {
+        return _entities.FindAsync(id).AsTask();
+    }
+    public Task<List<TEntity>> GetAllAsync() 
+    {
+        return _entities.ToListAsync();
+    }
     public void Add(TEntity entity)
     {
         _entities.Add(entity);
     }
     public void Remove(TEntity entity)
     {
-        if (_ctx.Entry(entity).State == EntityState.Detached) 
+        if (_dbContext.Entry(entity).State == EntityState.Detached) 
+        {
             _entities.Attach(entity);
+        }
 
         _entities.Remove(entity);
     }
-    public async Task RemoveAsync(int id) 
+    public async Task RemoveByIdAsync(int id) 
     {
         TEntity? entity = await GetByIdAsync(id).ConfigureAwait(false);
 
         if (entity != null) 
+        {
             Remove(entity);
+        }
     }
     public void Update(TEntity entity)
     {
         _entities.Attach(entity);
-        _ctx.Entry(entity).State = EntityState.Modified;
+        _dbContext.Entry(entity).State = EntityState.Modified;
     }
     public Task SaveAsync() 
     {
-        return _ctx.SaveChangesAsync();
+        return _dbContext.SaveChangesAsync();
     }
 
     public void Dispose() 
@@ -56,7 +66,7 @@ internal sealed class Repository<TEntity> : IRepository<TEntity>
         {
             if (disposing) 
             {
-                _ctx.Dispose();
+                _dbContext.Dispose();
             }
 
             _disposed = true;
