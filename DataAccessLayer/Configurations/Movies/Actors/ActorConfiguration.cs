fad318962a1eab96a48234eae9bf23b7fc8a6ce6
WesTer006 <162494366+WesTer006@users.noreply.github.com>
2025-01-26 14:55:05 +0200

    Feat/10/fluent configuration (#16)
    * set up dependencies
    
    * test
    
    * finished set up entities dependency

---
+++ DataAccessLayer/Configurations/Movies/Actors/ActorConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Models.Movies.Actors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Configurations
+{
+    public class ActorConfiguration : IEntityTypeConfiguration<Actor>
+    {
+        public void Configure(EntityTypeBuilder<Actor> builder)
+        {
+            builder.ToTable("Actors");
+
+            builder.HasKey(a => a.Id);
+
+            builder.Property(a => a.Firstname)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.Property(a => a.Lastname)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.HasMany(a => a.MovieActors)
+                   .WithOne(ma => ma.Actor)
+                   .HasForeignKey(ma => ma.ActorId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ DataAccessLayer/Configurations/Tickets/TicketConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess.Models.Tickets;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Configurations.Tickets;
+
+public class TicketConfiguration : IEntityTypeConfiguration<Ticket>
+{
+    public void Configure(EntityTypeBuilder<Ticket> builder)
+    {
+        builder.ToTable("Tickets");
+
+        builder.HasKey(t => t.Id);
+
+        builder.Property(t => t.Id)
+            .HasColumnName("id")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(t => t.BookingDate)
+            .HasColumnName("booking_date")
+            .IsRequired();
+
+        builder.HasOne(t => t.User)
+            .WithMany(u => u.Tickets)
+            .HasForeignKey(t => t.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(t => t.Session)
+            .WithMany(s => s.Tickets)
+            .HasForeignKey(t => t.SessionId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(t => t.Seat)
+            .WithMany()
+            .HasForeignKey(t => t.SeatId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
