4fa97fe79dbd76bc5ab57adc64e1b1fdf30a5013
WesTer <romanvs0602@gmail.com>
2025-01-26 14:25:57 +0200

    test

--- DataAccessLayer/Data/AppDbContext.cs
+++ DataAccessLayer/Data/AppDbContext.cs
@@ -1,32 +1,45 @@
-using DataAccess.Configurations;
 using DataAccess.Configurations.Sessions;
 using DataAccess.Configurations.Tickets;
+using DataAccess.Configurations;
+using DataAccess.Models.Movies;
+using DataAccess.Models.Movies.Actors;
+using DataAccess.Models.Sessions;
+using DataAccess.Models.Tickets;
 using DataAccess.Models.Users;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace DataAccessLayer.Data;
-
-public class AppDbContext : IdentityDbContext<AppUser>
+namespace DataAccessLayer.Data
 {
-    public AppDbContext() {}
-    
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-    protected override void OnModelCreating(ModelBuilder builder)
+    public class AppDbContext : IdentityDbContext<AppUser>
     {
-        base.OnModelCreating(builder);
+        public AppDbContext() { }
 
-        builder.ApplyConfiguration(new AppUserConfiguration());
-        builder.ApplyConfiguration(new TicketConfiguration());
-        builder.ApplyConfiguration(new SessionConfiguration());
-        builder.ApplyConfiguration(new HallConfiguration());
-        builder.ApplyConfiguration(new SeatConfiguration());
-        builder.ApplyConfiguration(new MovieConfiguration());
-        builder.ApplyConfiguration(new GenreConfiguration());
-        builder.ApplyConfiguration(new MovieGenreConfiguration());
-        builder.ApplyConfiguration(new ActorConfiguration());
-        builder.ApplyConfiguration(new MovieActorConfiguration());
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Hall> Halls { get; set; }
+        public DbSet<Seat> Seats { get; set; }
+        public DbSet<MovieActor> MovieActors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new AppUserConfiguration());
+            builder.ApplyConfiguration(new TicketConfiguration());
+            builder.ApplyConfiguration(new SessionConfiguration());
+            builder.ApplyConfiguration(new HallConfiguration());
+            builder.ApplyConfiguration(new SeatConfiguration());
+            builder.ApplyConfiguration(new MovieConfiguration());
+            builder.ApplyConfiguration(new GenreConfiguration());
+            builder.ApplyConfiguration(new MovieGenreConfiguration());
+            builder.ApplyConfiguration(new ActorConfiguration());
+            builder.ApplyConfiguration(new MovieActorConfiguration());
+        }
     }
-
 }
