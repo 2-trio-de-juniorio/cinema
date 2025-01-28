76fd011fba4ce7977e712bbedc670be403e4ca54
YuzkoDmytro <124136160+YuzkoDmytro@users.noreply.github.com>
2025-01-28 17:29:04 +0200

    done automapper

--- BusinessLogicLayer/Profiles/AuthProfile.cs
+++ BusinessLogicLayer/Profiles/AuthProfile.cs
@@ -3,7 +3,7 @@ using BusinessLogicLayer.DTOs;
 
 namespace BusinessLogicLayer.Profiles
 {
-    public class AuthProfile : AutoMapper.Profile
+    public class AuthProfile : Profile
     {
         public AuthProfile()
         {
