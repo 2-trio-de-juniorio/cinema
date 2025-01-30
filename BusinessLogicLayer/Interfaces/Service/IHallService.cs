53b76dc8577a337ca68a61f7472acf132c1a73ee
WesTer <romanvs0602@gmail.com>
2025-01-30 13:12:23 +0200

    add hall-controller

---
+++ BusinessLogicLayer/Interfaces/Service/IHallService.cs
@@ -0,0 +1,13 @@
+﻿using BusinessLogicLayer.Dtos;
+
+namespace BusinessLogicLayer.Interfaces
+{
+    public interface IHallService
+    {
+        Task<List<HallDto>> GetAllHallsAsync();
+        Task<HallDto?> GetHallByIdAsync(int id);
+        Task<int> CreateHallAsync(HallDto hallDto);
+        Task<bool> UpdateHallAsync(int id, HallDto hallDto);
+        Task<bool> DeleteHallAsync(int id);
+    }
+}
---
+++ BusinessLogicLayer/Services/HallService.cs
@@ -0,0 +1,91 @@
+﻿using BusinessLogicLayer.Dtos;
+using BusinessLogicLayer.Interfaces;
+using DataAccess;
+using DataAccess.Models.Sessions;
+using DataAccessLayer.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLogicLayer.Services;
+
+public class HallService : IHallService
+{
+    private readonly AppDbContext _context;
+
+    public HallService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<HallDto>> GetAllHallsAsync()
+    {
+        var halls = await _context.Halls
+            .Include(h => h.Seats)
+            .ToListAsync();
+
+        return halls.Select(hall => (HallDto)hall).ToList();
+    }
+
+    public async Task<HallDto?> GetHallByIdAsync(int id)
+    {
+        var hall = await _context.Halls
+            .Include(h => h.Seats)
+            .FirstOrDefaultAsync(h => h.Id == id);
+
+        return hall != null ? (HallDto)hall : null;
+    }
+
+    public async Task<int> CreateHallAsync(HallDto hallDto)
+    {
+        var hall = new Hall
+        {
+            Name = hallDto.Name,
+            Seats = hallDto.Seats.Select(seatDto => new Seat
+            {
+                RowNumber = seatDto.RowNumber,
+                SeatNumber = seatDto.SeatNumber,
+                IsBooked = seatDto.IsBooked
+            }).ToList()
+        };
+
+        _context.Halls.Add(hall);
+        await _context.SaveChangesAsync();
+        return hall.Id;
+    }
+
+    public async Task<bool> UpdateHallAsync(int id, HallDto hallDto)
+    {
+        var hall = await _context.Halls
+            .Include(h => h.Seats)
+            .FirstOrDefaultAsync(h => h.Id == id);
+
+        if (hall == null)
+        {
+            return false;
+        }
+
+        hall.Name = hallDto.Name;
+        hall.Seats = hallDto.Seats.Select(seatDto => new Seat
+        {
+            RowNumber = seatDto.RowNumber,
+            SeatNumber = seatDto.SeatNumber,
+            IsBooked = seatDto.IsBooked
+        }).ToList();
+
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<bool> DeleteHallAsync(int id)
+    {
+        var hall = await _context.Halls.FindAsync(id);
+
+        if (hall == null)
+        {
+            return false;
+        }
+
+        _context.Halls.Remove(hall);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+}
