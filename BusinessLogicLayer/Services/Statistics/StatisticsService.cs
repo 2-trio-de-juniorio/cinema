644fc75872e28cb4ae7c31da1291b897420fa94a
WesTer <romanvs0602@gmail.com>
2025-02-05 00:43:47 +0200

    changed StatisticsService

--- BusinessLogicLayer/Services/Statistics/StatisticsService.cs
+++ BusinessLogicLayer/Services/Statistics/StatisticsService.cs
@@ -2,12 +2,10 @@
 using DataAccess.Models.Tickets;
 using DataAccessLayer.Interfaces;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace BusinessLogicLayer.Services.Statistics
 {
-    /// <summary>
-    /// Service for retrieving statistics about ticket sales.
-    /// </summary>
     public class StatisticsService : IStatisticsService
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -19,16 +17,12 @@ namespace BusinessLogicLayer.Services.Statistics
             _logger = logger;
         }
 
-        /// <summary>
-        /// Gets the number of booked tickets for a specific movie.
-        /// </summary>
-        /// <param name="movieId">The ID of the movie.</param>
-        /// <returns>The number of booked tickets.</returns>
         public async Task<int> GetBookedTicketsCountAsync(int movieId)
         {
             _logger.LogInformation($"Fetching booked tickets count for movie ID: {movieId}");
 
             var ticketRepo = _unitOfWork.GetRepository<Ticket>();
+
             return await ticketRepo.CountAsync(t => t.Session.MovieId == movieId);
         }
     }
